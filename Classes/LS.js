d16c37d87d21e21f6f6870e4cc36057f7f0c5b51
UwUBeebi <chris.erman@khk.ee>
2021-10-18 15:23:10 +0300

    Refactored LS data getting and setting functionality

--- Classes/LS.js
+++ Classes/LS.js
@@ -1,5 +1,19 @@
 class LS {
-	
+
+	getData(name){
+		let data;
+		if(localStorage.getItem(name) === null){
+			data = [];
+		} else {
+			data = JSON.parse(localStorage.getItem(name));
+		}
+		return data;
+	}
+
+	setData(name, data){
+		localStorage.setItem(name, JSON.stringify(data));
+	}
+		
 	addTask(task){
 		let tasks;
 		if(localStorage.getItem('tasks') === null){
